8951cccdbdf0cb2a32475798340edfe7fd8a6403
Josh Stupplebeen <jpstup@gmail.com>
2018-05-01 00:50:17 -0600

    working on mod implementation

--- src/PartialPersistence.js
+++ src/PartialPersistence.js
@@ -1,16 +1,30 @@
+const applyMods = (init = [], mods = []) => {
+    const removeElement = (arr, el) => {
+    }
+
+    const arr = mods.reduce((acc, mod) => {
+        [modifier, element] = mod
+        if (modifier === '+') {
+            acc.push(element)
+        } else if (modifier === '-') {
+            removeElement(acc, element)
+        }
+
+        return acc
+    }, init)
+    return arr
+}
 
 const List = (init = []) => {
-    let persistentList = []
-    let internalList = []
-    let mod
+    let persistentList = init
 
     return {
-        append: (val) => {
-            persistentList.push(internalList)
-            internalList.push(val)
-            console.log('internalList: ', internalList)
-            console.log('persistentList: ', persistentList)
-            return internalList[internalList.length].concat([val])
+        push: (val) => {
+            // persistentList.push(internalList)
+            // internalList.push(val)
+            // console.log('internalList: ', internalList)
+            // console.log('persistentList: ', persistentList)
+            // return internalList[internalList.length].concat([val])
         },
         pop: () => [],
     }
---
+++ src/nodes.js
@@ -0,0 +1,25 @@
+
+// Partial Persistence API:
+// 1. Add mod
+// 2. Add new node
+
+class PartiallyPersistentNode {
+    constructor(isRoot, prev, next) {
+
+    }
+}
+
+const partiallyPersistentNode = {
+    next: 
+    add: () => {
+    },
+
+};
+
+const fullPersistentNode = Immutable.Map({
+
+});
+
+module.exports = {
+
+};
