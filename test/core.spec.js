d8e095ef2ba3d23644b2f54c1c56c9f598311264
Josh Stupplebeen <jpstup@gmail.com>
2018-05-09 00:39:04 -0600

    moar tests

--- test/core.spec.js
+++ test/core.spec.js
@@ -1,9 +1,12 @@
 import { List } from '../src/PartialPersistence'
 
 describe('Partially Persistent List', () => {
-    it('should add things', () => {
-        let foo = List([]);
-
+    it('should add things to an empty list', () => {
+        let foo = List([])
         expect(foo.push(1)).toEqual([1])
+    })
+    it('should add things to a non-empty list', () => {
+        let foo = List([1])
+        expect(foo.push(2)).toEqual([1, 2])
     })
 })
