245b5221e5310a39a1b3b8b77a7428a187f66885
Joshua Stupplebeen <jpstup@gmail.com>
2018-08-09 19:50:26 -0600

    switch to class impl

--- src/PartiallyPersistentList.js
+++ src/PartiallyPersistentList.js
@@ -1,62 +1,74 @@
 import { persistentNode } from './nodes'
 
-export const List = (initialList) => {
-  const rootNode = new persistentNode(initialList)
-  const lastNode = () => {
-    let node = rootNode
+export class List {
+  static rootNode
+
+  constructor(initialList) {
+    this.rootNode = new persistentNode(initialList || [])
+  }
+
+  lastNode() {
+    let node = this.rootNode
     while (node.next !== null) {
       node = node.next
     }
     return node
   }
 
-  return {
-    toJS: () => {
-      return lastNode().val
-    },
-    push: (addVal) => {
-      lastNode().next = new persistentNode(lastNode().val.concat([addVal]))
-      return lastNode().next
-    },
-    pop: () => {
-      if (lastNode().val.length === 0) {
-        throw 'Attempted to remove from empty list'
-      }
-
-      const lastVal = lastNode().val
-      while (lastVal.indexOf(removeVal) !== -1) {
-        lastVal.splice(lastVal.indexOf(removeVal), 1)
-      }
-      lastNode().next = new persistentNode(lastNode().val.slice())
-    },
-    head: () => {},
-    tail: () => {},
-    shift: () => {},
-    unshift: () => {},
-    slice: () => {},
-    splice: () => {},
-    inspect: () => {
-      return `Temporal.PartiallyPersistentList(${rootNode.val})`
-    },
-    tInspect: () => {
-      let node = rootNode
-      let out = ''
-      while (node !== null) {
-        out += node.toString()
-        node = node.next
-      }
-      return out
-    },
-    prev: (num) => {},
-    length: () => lastNode().val.length,
-    tLength: () => {
-      let node = rootNode,
-        length = 0
-      while (node.next !== null) {
-        node = node.next
-        length++
-      }
-      return length
-    },
+  toJS() {
+    return this.lastNode().val
   }
+
+  add(addVal) {
+    this.lastNode().next = new persistentNode(lastNode().val.concat([addVal]))
+    return this
+  }
+
+  remove(idx) {
+    if (lastNode().val.length === 0) {
+      throw 'Attempted to remove from empty list'
+    }
+
+    const lastVal = lastNode().val
+    lastVal.delete(idx)
+    lastNode().next = new persistentNode(lastVal)
+    return this
+  }
+  head() {}
+  tail() {}
+  shift() {}
+  unshift() {}
+  slice() {}
+  splice() {}
+  inspect() {
+    return `Temporal.PartiallyPersistentList(${lastNode().val || `null`})`
+  }
+  tInspect() {
+    let node = rootNode
+    let out = ''
+    while (node !== null) {
+      out += node.toString()
+      node = node.next
+    }
+    return out
+  }
+  prev(num) {}
+  length() {
+    return lastNode().val.length
+  }
+  tLength() {
+    let node = rootNode
+      length = 0
+    while (node.next !== null) {
+      node = node.next
+      length++
+    }
+    return length
+  }
+  map(f) {}
+  filter(f) {}
+  reduce(f) {}
+  tmap(f) {}
+  tfilter(f) {}
+  treduce(f) {}
 }
--- test/core.spec.js
+++ test/core.spec.js
@@ -1,39 +1,48 @@
 import { List } from '../src/PartiallyPersistentList'
 
 describe('Partially Persistent List', () => {
-  it('empty list constructor should return empty array', () => {
-    let foo = List([])
-    expect(foo.toJS()).toEqual([])
+  describe('constructor', () => {
+    it('with no elements should return empty array', () => {
+      let foo = new List([])
+      expect(foo.toJS()).toEqual([])
+    })
+    it('with an element and toJS work', () => {
+      let foo = new List([1])
+      expect(foo.toJS()).toEqual([1])
+    })
   })
-  it('list constructor and toJS work', () => {
-    let foo = List([1])
-    expect(foo.toJS()).toEqual([1])
+
+  describe('should add things', () => {
+    it('to an empty list', () => {
+      let foo = new List([])
+      const bar = foo.add(1).add(1)
+      console.log('bar: ', bar)
+      // expect(foo.add(1)).toEqual(`Temporal.PartiallyPersistentList([1])`)
+    })
+    // it('to a non-empty list', () => {
+    //   let foo = List([1])
+    //   expect(foo.add(2)).toEqual([1, 2])
+    // })
   })
-  it('should add things to an empty list', () => {
-    let foo = List([])
-    expect(foo.push(1)).toEqual([1])
-  })
-  it('should add things to a non-empty list', () => {
-    let foo = List([1])
-    expect(foo.push(2)).toEqual([1, 2])
-  })
-  //   need to work on removal api
+
+  // describe('should remove things', () => {
   //   it('removing from an empty list should throw an exception', () => {
   //     let foo = List([])
   //     expect(foo.pop).toThrow('Attempted to remove from empty list')
   //   })
-  it('empty list should return length of 0', () => {
-    let foo = List([])
-    expect(foo.length()).toEqual(0)
-  })
-  it('non empty list should return non-zero length', () => {
-    let foo = List([1])
-    expect(foo.length()).toEqual(1)
-    expect(foo.push(2).length()).toEqual(2)
-  })
-  it('empty list should return tLength of 1', () => {})
-  it('popping from a non-empty list should return the list without the last pushed value', () => {})
-  it('length should return the length', () => {})
-  it('should show all past values upon inspection', () => {})
-  it("tLength should return the number of values back to the variable's initalization", () => {})
+  // })
+  // it('empty list should return length of 0', () => {
+  //   let foo = List([])
+  //   expect(foo.length()).toEqual(0)
+  // })
+  // it('non empty list should return non-zero length', () => {
+  //   let foo = List([1])
+  //   expect(foo.length()).toEqual(1)
+  //   expect(foo.add(2).length()).toEqual(2)
+  // })
+  // it('empty list should return tLength of 1', () => {})
+  // it('popping from a non-empty list should return the list without the last pushed value', () => {})
+  // it('length should return the length', () => {})
+  // it('should show all past values upon inspection', () => {})
+  // it("tLength should return the number of values back to the variable's initalization", () => {})
 })
